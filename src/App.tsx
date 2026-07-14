a63ba18594479c3daee09dcf696bae64136fd5fd
krishanamondal <mail.krishanamondal@gmail.com>
2026-07-14 19:52:28 +0600

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='p-10 mt-5 '>
+    <h1 className='text-3xl font-bold'>Hello World</h1>
+   </div>
+  )
+}
+
+export default App
